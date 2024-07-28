686926b5dc4e0f11134bd7427041948b607a2c86
Naganathan Paramasivam <93266258+naganathan01@users.noreply.github.com>
2024-07-28 22:09:09 +0530

    Add files via upload

---
+++ data.tf
@@ -0,0 +1,18 @@
+data "aws_caller_identity" "current" {}
+
+
+
+data "aws_iam_policy_document" "cluster_assume_role_policy" {
+  statement {
+    sid = "EKSClusterAssumeRole"
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
